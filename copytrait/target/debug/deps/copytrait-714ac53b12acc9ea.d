3da5b6532caed8b008368e2519ededa0e17a9de7
kn-shimada <xianrendaotian012@gmail.com>
2021-07-05 02:09:54 +0900

    add: remake - copytrait

---
+++ copytrait/target/debug/deps/copytrait-714ac53b12acc9ea.d
@@ -0,0 +1,5 @@
+c:\Users\xianr\Files\Rust-learning\copytrait\target\debug\deps\copytrait-714ac53b12acc9ea.rmeta: src\main.rs
+
+c:\Users\xianr\Files\Rust-learning\copytrait\target\debug\deps\copytrait-714ac53b12acc9ea.d: src\main.rs
+
+src\main.rs:
---
+++ copytrait/target/debug/deps/copytrait-b19c488888073ad6.d
@@ -0,0 +1,5 @@
+c:\Users\xianr\Files\Rust-learning\copytrait\target\debug\deps\copytrait-b19c488888073ad6.rmeta: src\main.rs
+
+c:\Users\xianr\Files\Rust-learning\copytrait\target\debug\deps\copytrait-b19c488888073ad6.d: src\main.rs
+
+src\main.rs:
